0f57f4afba4b19c95041ce58129054a6d5eaf558
D1ogooo <diiogocosta11@gmail.com>
2024-08-08 02:13:17 -0300

    feat: add Header e banner adicionado

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
 "use client"
 
-import { useState } from "react"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
 
 const Home = () => {
-  const [] = useState()
-  return <Button>Bom dia</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Diogo!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[9.375rem] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/Banner_01.svg"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
