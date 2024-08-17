9a8d978d7429906a37a2c081048c1ed9f5ee1d7b
D1ogooo <diiogocosta11@gmail.com>
2024-08-17 10:33:39 -0300

    chore: add next auth setup

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetContent, SheetHeader, SheetTitle, SheetClose } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/seach"
-import { Avatar, AvatarImage } from "./ui/avatar"
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "./ui/dialog"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 
 const SidebarButton = () => {
   return (
@@ -14,7 +22,32 @@ const SidebarButton = () => {
       </SheetHeader>
 
       <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+        <h2 className="font-bold">Olá, faça seu login</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                alt="fazer login com o google"
+                src="/googleIcon.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage
             height={50}
             width={50}
@@ -25,7 +58,7 @@ const SidebarButton = () => {
         <div className="ml-3 flex flex-col">
           <p className="font-bold">Diogo maçal</p>
           <p className="text-xs">diiogomarsalcosta@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
