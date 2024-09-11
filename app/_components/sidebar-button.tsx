acfd376dfa4a7b2a816cac55fb7ebcdaab452dc5
D1ogooo <diiogocosta11@gmail.com>
2024-09-11 14:08:08 -0300

    feat: ensure user can search for barbershops

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "./ui/button"
@@ -12,9 +14,15 @@ import {
   DialogTitle,
   DialogDescription,
 } from "./ui/dialog"
-// import { Avatar, AvatarImage } from "./ui/avatar"
+import { signIn, useSession } from "next-auth/react"
+import { Avatar, AvatarImage } from "./ui/avatar"
 
 const SidebarButton = () => {
+  const { data } = useSession()
+  async function handleLoginWithGoogleClick() {
+    await signIn("google")
+  }
+
   return (
     <SheetContent>
       <SheetHeader>
@@ -22,43 +30,56 @@ const SidebarButton = () => {
       </SheetHeader>
 
       <div className="flex items-center gap-3 border-b border-solid py-5">
-        <h2 className="font-bold">Olá, faça seu login</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size="icon">
-              <LogInIcon />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%]">
-            <DialogHeader>
-              <DialogTitle>Faça login na plataforma</DialogTitle>
-              <DialogDescription>
-                Conecte-se usando sua conta do Google
-              </DialogDescription>
-            </DialogHeader>
-            <Button variant="outline" className="gap-1 font-bold">
-              <Image
-                alt="fazer login com o google"
-                src="/googleIcon.svg"
-                width={18}
-                height={18}
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage
+                height={50}
+                width={50}
+                src={data?.user?.image ?? ""}
               />
-              Google
-            </Button>
-          </DialogContent>
-        </Dialog>
-        {/* <Avatar>
-          <AvatarImage
-            height={50}
-            width={50}
-            src="https://images.unsplash.com/photo-1723477450561-791fd67efa56?q=80&w=388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
-        </Avatar>
+            </Avatar>
 
-        <div className="ml-3 flex flex-col">
-          <p className="font-bold">Diogo maçal</p>
-          <p className="text-xs">diiogomarsalcosta@gmail.com</p>
-        </div> */}
+            <div className="ml-3 flex flex-col">
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            {" "}
+            <h2 className="font-bold">Olá, faça seu login</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <DialogHeader>
+                  <DialogTitle>Faça login na plataforma</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta do Google
+                  </DialogDescription>
+                </DialogHeader>
+
+                <Button
+                  variant="outline"
+                  className="gap-1 font-bold"
+                  onClick={handleLoginWithGoogleClick}
+                >
+                  <Image
+                    alt="fazer login com o google"
+                    src="/googleIcon.svg"
+                    width={18}
+                    height={18}
+                  />
+                  Google
+                </Button>
+              </DialogContent>
+            </Dialog>{" "}
+          </>
+        )}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Footer from "./_components/footer"
+import AuthProvider from "./_providers/auth"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,10 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="en" className="dark">
       <body className={inter.className}>
-        {children}
-        <Footer />
+        <AuthProvider>
+          <div className="flex h-full flex-col">
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </div>
+        </AuthProvider>
       </body>
     </html>
   )
